4559fd618a676b2e132ed7e3c09a08d351649e0d
dave <concaveNP@gmail.com>
2015-10-30 00:59:35 -0600

    Code Formatting

--- app/src/main/java/com/concavenp/nanodegree/nanodegreeapps/MainActivity.java
+++ app/src/main/java/com/concavenp/nanodegree/nanodegreeapps/MainActivity.java
@@ -1,13 +1,11 @@
 package com.concavenp.nanodegree.nanodegreeapps;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -24,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         spotifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This will launch my: " + getResources().getString(R.string.spotify_streamer) + " app!" , Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "This will launch my: " + getResources().getString(R.string.spotify_streamer) + " app!", Toast.LENGTH_SHORT).show();
             }
         });
 
